8c6a0dfd3eb2a1b09a08283f545b5d41bfe520d9
Dean Liu <deanliu@gmail.com>
2015-12-07 18:08:29 -0800

    Adding axis

--- draw.js
+++ draw.js
@@ -11,6 +11,8 @@ d3.json("content.json",
     }
 );
 
+
+// Draw data
 function data_viz(incoming_data) {
     incoming_data.forEach(function (el){
         el.run_time = new Date(el.start_date_local)
@@ -32,10 +34,11 @@ function data_viz(incoming_data) {
 
     var color_scale = d3.scale.linear().domain([0, max_distance_miles]).range(["white", "#990000"])
     var time_ramp = d3.time.scale().domain(start_end).range([20,1350]);
+    var x_scale = time_ramp
     var y_scale = d3.scale.linear().domain([0, max_average_speed]).range([-550,460])
     var radius_scale = d3.scale.linear().domain([0, max_distance_miles]).range([1,20])
 
-    d3.select("svg")
+    var runG = d3.select("svg")
         .selectAll("circle")
         .data(incoming_data)
         .enter()
@@ -46,8 +49,7 @@ function data_viz(incoming_data) {
         .style("stroke", "black")
         .style("stroke-width", "1px")
         .style("fill", function(d) {return color_scale(d.distance_miles)})
-        
-
+    
 
     d3.select("body").selectAll("div.cities")
         .data(run_data)
@@ -55,6 +57,18 @@ function data_viz(incoming_data) {
         .append("div")
         .attr("class","runs")
         .html(function(d,i) { return d.name; })
+
+    // Draw axis
+    var y_axis = d3.svg.axis().scale(y_scale).orient("right")
+    d3.select("svg").append("g").attr("id", "yAxisG").call(y_axis)
+    var x_axis = d3.svg.axis().scale(x_scale).orient("bottom")
+    d3.select("svg").append("g").attr("id", "xAxisG").call(x_axis)
+    d3.selectAll("path.domain").style("fill", "none").style("stroke", "black");
+    d3.selectAll("line").style("stroke", "black");
 }
+
+
+
+
 
 
