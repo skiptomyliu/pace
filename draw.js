7cc4135cb58c2a26bb58b4aa4f3ed3e2dc38f711
Dean Liu <deanliu@gmail.com>
2015-12-08 14:06:56 -0800

    Highlight red Code reorg

--- draw.js
+++ draw.js
@@ -45,23 +45,46 @@ function data_viz(incoming_data) {
         // .nice(d3.time.week)
         .range([margin,w-margin]);
     var x_scale = time_ramp
-    // var y_scale = d3.scale.linear().domain([0, max_average_speed]).range([-(h+margin),h])
-    console.log(max_average_speed)
     var y_scale = d3.scale.linear().domain([min_average_speed, max_average_speed]).range([500, 0])
     var radius_scale = d3.scale.linear().domain([0, max_distance_miles]).range([1,20])
 
-    var runG = d3.select("svg")
-        .selectAll("circle")
+    d3.select("svg")    
+        .append("g")
+        .attr("id", "runsG")
+        .selectAll("g")
         .data(incoming_data)
         .enter()
-        .append("circle")   
+        .append("g")
+        .attr("class", "overallG")
+        .attr("transform", function(d,i) {
+            return "translate("+time_ramp(d.run_time)+","+y_scale(d.average_min_per_mi)+")"
+        })
+        
+    var runG = d3.selectAll("g.overallG")
+    runG.append("circle")
         .attr("r", function(d) {return radius_scale(d.distance_miles)})
-        .attr("cx", function(d,i) {return time_ramp(d.run_time);})
-        .attr("cy", function(d) {return y_scale(d.average_min_per_mi);})
         .style("stroke", "black")
         .style("stroke-width", "1px")
         .style("fill", function(d) {return color_scale(d.distance_miles)})
-    
+
+    runG.on("mouseover", highlightRegion);
+    function highlightRegion(d) {
+        d3.select(d3.event.target).transition().duration(500)
+            .style("fill", function(){return "red"})
+    }
+
+    runG.on("mouseout", function(){
+        d3.select(d3.event.target)
+            .style("fill", function(d){return color_scale(d.distance_miles)})
+    });
+
+
+
+    // runG.append("text")
+    //     .style("text-anchor", "middle")
+    //     .attr("y", 30)
+    //     .style("font-size", "10px")
+    //     .text(function(d) {return d.name});
 
     d3.select("body").selectAll("div.cities")
         .data(run_data)
