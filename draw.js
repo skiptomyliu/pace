52e06d2aa122f34ef14d4f17b167113c112227e5
Dean Liu <deanliu@gmail.com>
2015-12-09 20:20:24 -0800

    tooltip positioning

--- draw.js
+++ draw.js
@@ -68,6 +68,8 @@ function data_viz(incoming_data) {
         .style("fill", function(d) {return color_scale(d.distance_miles)})
 
     runG.on("mouseover", highlightRegion);
+
+
     function highlightRegion(d) {
         d3.select(d3.event.target).classed("active",true)
         // d3.select(d3.event.target).transition().duration(500)
@@ -77,16 +79,16 @@ function data_viz(incoming_data) {
         var x = coord[0]
         var y = coord[1]
 
-        var date_time = (d.run_time.getMonth()+1)+"/"+d.run_time.getDate() + "/" + d.run_time.getFullYear()
-        d3.select("#tooltip")
-            .style("left", x + "px")
-            .style("top", y + "px")
+        // Pop out the tooltip
+        var tooltip = d3.select("#tooltip")
 
-        d3.select("#tooltip #run_title a")
+        d3.select("#tooltip #run_title")
             .text(d.name)
-            .attr({"xlink:href": "http://www.skiptomyliu.com"})
-            // .on("click", function(d){alert('hi')})
 
+        d3.select("#tooltip a")
+            .attr({"href": "https://strava.com/activities/"+d.id})
+            
+        var date_time = (d.run_time.getMonth()+1)+"/"+d.run_time.getDate() + "/" + d.run_time.getFullYear()
         d3.select("#tooltip #run_date")
            .text(date_time)
         d3.select("#tooltip #run_distance")
@@ -94,6 +96,12 @@ function data_viz(incoming_data) {
         d3.select("#tooltip #run_pace")
            .text(d.average_min_per_mi.toPrecision(3))
         d3.select("#tooltip").classed("hidden", false);
+
+        var tooltipRect = tooltip.node().getBoundingClientRect()
+
+        d3.select("#tooltip")
+            .style("left", (x-tooltipRect.width/2.01) + "px")
+            .style("top", y-tooltipRect.height + "px")
     }
 
     runG.on("mouseout", function(){
@@ -148,9 +156,4 @@ function data_viz(incoming_data) {
       .attr("x2", x_scale);
 
 }
-
-
-
-
-
 
